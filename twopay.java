558dae0364dd8bccd7002e348b5c694dd47f1fcb
rayjay <devopsray.g@gmail.com>
2022-03-12 09:30:32 +0000

    twopay app created

---
+++ twopay.java
@@ -0,0 +1,9 @@
+echo "please enter your username"
+read username
+echo "please enter your password"
+read password
+echo "please confirm your password"
+read password
+echo "welcome to twowaypay app"
+date
+
